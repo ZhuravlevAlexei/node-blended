53a2c2ccff6be3ce767b39d7c21db60947ac11d6
Данил <danikkach666@gmail.com>
2024-05-25 12:23:36 +0300

    add

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -9,11 +9,11 @@ export const initMongoConnection = async () => {
     const db = env("MONGODB_DB");
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority`
+      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=M0DanilBoboshko`
     );
     console.log("Mongo connection successfully established!");
-  } catch (e) {
-    console.log("Error while setting up mongo connection", e);
-    throw e;
+  } catch (error) {
+    console.log("Error while setting up mongo connection", error);
+    throw error;
   }
 };
